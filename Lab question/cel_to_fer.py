1e43659fb5a2a879a30e81bbe991e33c829b4c5f
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-02-19 11:27:03 +0530

    First commit

---
+++ Lab question/cel_to_fer.py
@@ -0,0 +1,18 @@
+# ---->  question one 
+# 1.Python program to convert the temperature in degree centigrade to Fahrenheit
+
+
+celsius =32
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
+
+
+# ---->  question two 
+
+# 2. Python program to find the area of a triangle whose sides are given
+
+base=10
+height=23
+
+triangle_area = 0.5 * base * height
+print(f"The area of the triangle is: {triangle_area:.2f} ")
