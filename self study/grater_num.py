1d4da4465d4433155c033401a0d682f390f3be95
RiteshPanchal279 <riteshpanchal279@gmail.com>
2025-02-19 13:05:29 +0530

    self study up

--- self study/grater_num.py
+++ self study/grater_num.py
@@ -1,24 +1,24 @@
+# finding the grater form Four without using logical operator 
 num1=223
 num2=344
-num3=100
-num4=53
+num3=102
+num4=530
 
 if(num1>num2):
-   print("on")
-
-
-
-#    if(num1>num2):
-#     if(num1>num3):
-#         print("Greatest no: ", num1)
-#     else:
-#         print("Greatest no:",num3)
-# else:
-#     if(num2>num3):
-#         print("Greatest no: ", num2)
-#     else:
-#         print("Greatest no:",num3)
-
-
+   if(num1>num3):
+      if(num1>num4):
+         print("Greatest no: ", num1)
+      else:
+         print("Greatest no: ", num4)
+   else:
+      print("Greatest no: ", num3)
+else:
+   if(num2>num3):
+      if(num2>num4):
+         print("Greatest no: ", num2)
+      else:
+         print("Greatest no: ", num4)
+   else:
+      print("Greatest no: ", num3)
 
    
